33be4bbcf9f97393056302a5342081819a7162c0
VladDrobitko <boss.drobitko@gmail.com>
2024-08-12 22:01:16 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Владислав Дробитько on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
